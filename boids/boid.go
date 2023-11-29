a3a9cbf1a4fe3b1d3835252a12eb2d6c61b9dd00
Akshansh Gusain <akshanshgusain@gmail.com>
2023-11-29 20:17:16 +0530

    add ebiten, Draw func

--- boids/boid.go
+++ boids/boid.go
@@ -1,7 +1,20 @@
 package boids
 
+import "math/rand"
+
 type Boid struct {
 	position Vector2D
 	velocity Vector2D
 	id       int
+}
+
+func createBoid(id int) {
+	b := Boid{
+		position: Vector2D{rand.Float64() * screenWidth, rand.Float64() * screenHeight},
+		velocity: Vector2D{(rand.Float64() * 2) - 1, (rand.Float64() * 2) - 1},
+		id:       id,
+	}
+
+	boids[id] = &b
+	go b.start()
 }
--- boids/main.go
+++ boids/main.go
@@ -1,7 +1,50 @@
 package boids
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/hajimehoshi/ebiten/v2"
+	"image/color"
+	"log"
+)
+
+const (
+	screenWidth, screenHeight = 640, 360
+	boidCount                 = 500
+)
+
+var (
+	green = color.RGBA{10, 255, 50, 255}
+	boids [boidCount]*Boid
+)
+
+type Game struct {
+}
+
+func (g *Game) Update() error { return nil }
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	for _, boid := range boids {
+		screen.Set(int(boid.position.x+1), int(boid.position.y), green)
+		screen.Set(int(boid.position.x-1), int(boid.position.y), green)
+		screen.Set(int(boid.position.x), int(boid.position.y+1), green)
+		screen.Set(int(boid.position.x), int(boid.position.y-1), green)
+	}
+}
+
+func (g *Game) Layout(_, _ int) (w, h int) {
+	return screenWidth, screenHeight
+}
 
 func Run() {
 	fmt.Println("Running Boids")
+
+	for i := 0; i < boidCount; i++ {
+		createBoid(i)
+	}
+
+	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+	ebiten.SetWindowTitle("Boids")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
