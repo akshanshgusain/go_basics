2a2d3330ce615928e72c06b642ed42a6167b6b72
Akshansh Gusain <akshanshgusain@gmail.com>
2023-11-30 20:35:29 +0530

    add start, moveOne funcs

--- boids/boid.go
+++ boids/boid.go
@@ -1,11 +1,32 @@
 package boids
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 type Boid struct {
 	position Vector2D
 	velocity Vector2D
 	id       int
+}
+
+func (b *Boid) moveOne() {
+	b.position = b.position.Add(b.velocity)
+	next := b.position.Add(b.velocity)
+	if next.x >= screenWidth || next.x < 0 {
+		b.velocity = Vector2D{-b.velocity.x, b.velocity.y}
+	}
+	if next.y >= screenHeight || next.y < 0 {
+		b.velocity = Vector2D{b.velocity.x, -b.velocity.y}
+	}
+}
+
+func (b *Boid) start() {
+	for {
+		b.moveOne()
+		time.Sleep(5 * time.Millisecond)
+	}
 }
 
 func createBoid(id int) {
