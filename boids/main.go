f8b04590cc006fdf8ec2c5b2f80379da1a8eb350
Akshansh Gusain <akshanshgusain@gmail.com>
2023-12-01 15:48:36 +0530

    initilise boid map

--- boids/main.go
+++ boids/main.go
@@ -10,11 +10,14 @@ import (
 const (
 	screenWidth, screenHeight = 640, 360
 	boidCount                 = 500
+	viewRadius                = 12
+	adjRate                   = 0.015
 )
 
 var (
-	green = color.RGBA{10, 255, 50, 255}
-	boids [boidCount]*Boid
+	green   = color.RGBA{10, 255, 50, 255}
+	boids   [boidCount]*Boid
+	boidMap [screenWidth + 1][screenHeight + 1]int
 )
 
 type Game struct {
@@ -37,6 +40,13 @@ func (g *Game) Layout(_, _ int) (w, h int) {
 
 func Run() {
 	fmt.Println("Running Boids")
+	fmt.Println("Initialising Boid Map")
+	for i, row := range boidMap {
+		for j := range row {
+			boidMap[i][j] = -1
+		}
+
+	}
 
 	for i := 0; i < boidCount; i++ {
 		createBoid(i)
